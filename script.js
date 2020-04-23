9db59823f56af31cec31be3dbac1b465a29052f5
“Ros” <rkudybyn@gmail.com>
2020-04-23 16:11:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// let persone={
+//     name:"John",
+//     age:24,
+//     isMarried:false,
+// };
+// console.log(persone["name"]);
+
+// let arr=['plum.png','orange.jpg','apple.bmp']
+// console.log(arr[0]);
+
+//alert ("Hello world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+// let answer = +prompt ("есть ли вам 18?", "ДА");
+
+// console.log(typeof(answer));
+
+// console.log("arr"+"-object"); 
+
+// console.log( 4+"-object");
+
+let incr= 10,
+    decr= 10;
+
+
+console.log (incr++);
+console.log (decr--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
+let isChecked=false;
+let isClose=false;
+
+console.log(isChecked || !isClose);
